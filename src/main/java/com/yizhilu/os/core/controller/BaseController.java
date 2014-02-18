b7fbe953e7459eb19a528907aa8671f4b30e764e
fairyhawk <bis@foxmail.com>
2014-02-18 13:13:11 +0800

    加注释

--- src/main/java/com/yizhilu/os/core/controller/BaseController.java
+++ src/main/java/com/yizhilu/os/core/controller/BaseController.java
@@ -33,188 +33,198 @@ import com.yizhilu.os.core.util.web.WebUtils;
  */
 public class BaseController {
 
-	// log对象
-	private static final Logger logger = Logger.getLogger(BaseController.class);
-	public static Gson gson = new GsonBuilder().setDateFormat(
-			"yyyy-MM-dd HH:mm:ss").create();
-	public static JsonParser jsonParser = new JsonParser();
-	private PageEntity page;// 分页数据单独保存
+    // log对象
+    private static final Logger logger = Logger.getLogger(BaseController.class);
+    // gson日期默认格式设置
+    public static Gson gson = new GsonBuilder().setDateFormat("yyyy-MM-dd HH:mm:ss")
+            .create();
+    public static JsonParser jsonParser = new JsonParser();
+    private PageEntity page;// 分页数据单独保存
 
-	public Map<String, Object> json = new HashMap<String, Object>();
+    public Map<String, Object> json = new HashMap<String, Object>();
 
-	// 公用返回路径
-	public String login = "/login";
-	public String reLogin = "/suser/jsp/login/loginOut";
-	public String loginError = "login!loginInput.do?rdtType=1";
-	public String limitVerifyError = "/suser/jsp/login/limitVerifyError.jsp";
-	public String changeSuccess = "/admin/common/success";// 修改成功提示页面
+    // 公用返回路径
+    public String login = "/login";
+    public String reLogin = "/suser/jsp/login/loginOut";
+    public String loginError = "login!loginInput.do?rdtType=1";
+    public String limitVerifyError = "/suser/jsp/login/limitVerifyError.jsp";
+    public String changeSuccess = "/admin/common/success";// 修改成功提示页面
 
-	public void setJson(boolean success, String message, Object entity) {
-		json.put("success", success);
-		json.put("message", message);
-		json.put("entity", entity);
-	}
+    /**
+     * 封装Json返回信息
+     * 
+     * @param success
+     *            是否操作成功
+     * @param message
+     *            返回辅助信息
+     * @param entity
+     *            返回数据时实体
+     */
+    public void setJson(boolean success, String message, Object entity) {
+        json.put("success", success);
+        json.put("message", message);
+        json.put("entity", entity);
+    }
 
-	// 绑定变量名字和属性，参数封装进类
-	@InitBinder("page")
-	public void initBinderPage(WebDataBinder binder) {
-		binder.setFieldDefaultPrefix("page.");
-	}
+    // 绑定变量名字和属性，参数封装进类
+    @InitBinder("page")
+    public void initBinderPage(WebDataBinder binder) {
+        binder.setFieldDefaultPrefix("page.");
+    }
 
-	@Getter
-	@Setter
-	protected String errmsg;// 错误信息
+    @Getter
+    @Setter
+    protected String errmsg;// 错误信息
 
-	protected static final String ERROR = "/error/error";// 前台错误信息
-	protected static final String ADMIN_ERROR = "/admin/error/error";// 后台错误信息
-	protected static String ADMIN_SUCCESS = "redirect:/admin/sys/success";// 后台提交成功
+    protected static final String ERROR = "/error/error";// 前台错误信息
+    protected static final String ADMIN_ERROR = "/admin/error/error";// 后台错误信息
+    protected static String ADMIN_SUCCESS = "redirect:/admin/sys/success";// 后台提交成功
 
-	public String getUuid() {
-		return UUID.randomUUID().toString();
-	}
+    public String getUuid() {
+        return UUID.randomUUID().toString();
+    }
 
-	/**
-	 * 将错误信息放到页面中,后台用
-	 */
-	public String setExceptionRequestAdmin(HttpServletRequest request,
-			Exception e) {
-		StackTraceElement[] messages = e.getStackTrace();
-		if (!ObjectUtils.isNull(messages)) {
-			StringBuffer buffer = new StringBuffer();
-			buffer.append(e.toString()).append("<br/>");
-			for (int i = 0; i < messages.length; i++) {
-				/*
-				 * buffer.append(messages[i].getClassName());
-				 * buffer.append(messages[i].getMethodName()).append("(");
-				 * buffer.append(messages[i].getFileName()).append(":");
-				 * buffer.append(messages[i].getLineNumber()).append(")");
-				 */
-				buffer.append(messages[i].toString()).append("<br/>");
-			}
-			request.setAttribute("myexception", buffer.toString());
-		}
-		return ADMIN_ERROR;
-	}
+    /**
+     * 
+     * 将错误信息放到页面中,后台用
+     * 
+     * @param request
+     * @param e
+     * @return
+     */
+    public String setExceptionRequestAdmin(HttpServletRequest request, Exception e) {
+        StackTraceElement[] messages = e.getStackTrace();
+        if (!ObjectUtils.isNull(messages)) {
+            StringBuffer buffer = new StringBuffer();
+            buffer.append(e.toString()).append("<br/>");
+            for (int i = 0; i < messages.length; i++) {
+                buffer.append(messages[i].toString()).append("<br/>");
+            }
+            request.setAttribute("myexception", buffer.toString());
+        }
+        return ADMIN_ERROR;
+    }
 
-	/**
-	 * 将错误信息放到页面中,前台用
-	 */
-	public String setExceptionRequest(HttpServletRequest request, Exception e) {
-		StackTraceElement[] messages = e.getStackTrace();
-		if (!ObjectUtils.isNull(messages)) {
-			StringBuffer buffer = new StringBuffer();
-			buffer.append(e.toString()).append("<br/>");
-			for (int i = 0; i < messages.length; i++) {
-				buffer.append(messages[i].toString()).append("<br/>");
-			}
-			request.setAttribute("myexception", buffer.toString());
-		}
-		return ERROR;
-	}
+    /**
+     * 将错误信息放到页面中,前台用
+     * 
+     * @param request
+     * @param e
+     * @return
+     */
+    public String setExceptionRequest(HttpServletRequest request, Exception e) {
+        StackTraceElement[] messages = e.getStackTrace();
+        if (!ObjectUtils.isNull(messages)) {
+            StringBuffer buffer = new StringBuffer();
+            buffer.append(e.toString()).append("<br/>");
+            for (int i = 0; i < messages.length; i++) {
+                buffer.append(messages[i].toString()).append("<br/>");
+            }
+            request.setAttribute("myexception", buffer.toString());
+        }
+        return ERROR;
+    }
 
-	/**
-	 * session中设置值
-	 * 
-	 * @param request
-	 * @param name
-	 * @param v
-	 */
-	public void setSessionAttribute(HttpServletRequest request, String name,
-			Object v) {
-		request.getSession().setAttribute(name, v);
-	}
+    /**
+     * session中设置值
+     * 
+     * @param request
+     * @param name
+     * @param v
+     */
+    public void setSessionAttribute(HttpServletRequest request, String name, Object v) {
+        request.getSession().setAttribute(name, v);
+    }
 
-	/**
-	 * session中取值
-	 * 
-	 * @param request
-	 * @param name
-	 * @return
-	 */
-	public Object getSessionAttribute(HttpServletRequest request, String name) {
-		HttpSession session = getSession(request, false);
-		return (session != null ? session.getAttribute(name) : null);
-	}
+    /**
+     * session中取值
+     * 
+     * @param request
+     * @param name
+     * @return
+     */
+    public Object getSessionAttribute(HttpServletRequest request, String name) {
+        HttpSession session = getSession(request, false);
+        return (session != null ? session.getAttribute(name) : null);
+    }
 
-	/**
-	 * 取得HttpSession的简化函数.
-	 * 
-	 * @param request
-	 * @return HttpSession
-	 */
-	public static HttpSession getSession(HttpServletRequest request) {
-		return request.getSession();
-	}
+    /**
+     * 取得HttpSession的简化函数.
+     * 
+     * @param request
+     * @return HttpSession
+     */
+    public static HttpSession getSession(HttpServletRequest request) {
+        return request.getSession();
+    }
 
-	/**
-	 * 取得HttpSession的简化函数.
-	 * 
-	 * @param request
-	 * @return HttpSession
-	 */
-	public static HttpSession getSession(HttpServletRequest request,
-			boolean isNew) {
-		return request.getSession(isNew);
-	}
+    /**
+     * 取得HttpSession的简化函数.
+     * 
+     * @param request
+     * @return HttpSession
+     */
+    public static HttpSession getSession(HttpServletRequest request, boolean isNew) {
+        return request.getSession(isNew);
+    }
 
-	/**
-	 * 发送内容到response中
-	 * 
-	 * @param request
-	 * @param response
-	 * @param content
-	 * @throws IOException
-	 */
-	public void sendMessage(HttpServletRequest request,
-			HttpServletResponse response, String content) throws IOException {
-		try {
-			response.setCharacterEncoding("UTF-8");
-			response.getWriter().write(content);
-		} catch (Exception e) {
-			logger.error("sendMessage", e);
-		}
-	}
+    /**
+     * 发送内容到response中
+     * 
+     * @param request
+     * @param response
+     * @param content
+     * @throws IOException
+     */
+    public void sendMessage(HttpServletRequest request, HttpServletResponse response,
+            String content) throws IOException {
+        try {
+            response.setCharacterEncoding("UTF-8");
+            response.getWriter().write(content);
+        } catch (Exception e) {
+            logger.error("sendMessage", e);
+        }
+    }
 
-	/**
-	 * @return the page
-	 */
-	public PageEntity getPage() {
-		if (page == null) {
-			page = new PageEntity();
-		}
-		return page;
-	}
+    /**
+     * @return the page
+     */
+    public PageEntity getPage() {
+        if (page == null) {
+            page = new PageEntity();
+        }
+        return page;
+    }
 
-	/**
-	 * @param page
-	 *            the page to set
-	 */
-	public void setPage(PageEntity page) {
-		if (page != null) {
-			this.page = page;
-		}
-	}
+    /**
+     * @param page
+     *            the page to set
+     */
+    public void setPage(PageEntity page) {
+        if (page != null) {
+            this.page = page;
+        }
+    }
 
-	/**
-	 * 过滤 script 脚本攻击
-	 * 
-	 * @param resquest
-	 * @return
-	 * @throws Exception
-	 */
-	@SuppressWarnings("unchecked")
-	protected void replaceScript(HttpServletRequest resquest) throws Exception {
-		Enumeration<String> e = resquest.getParameterNames();
-		while (e.hasMoreElements()) {
-			String str = (String) e.nextElement();
-			String value = resquest.getParameter(str);
-			if (value.indexOf("script") != -1
-					|| value.indexOf("javascript") != -1
-					|| value.indexOf("eval") != -1) {
-				String ip = WebUtils.getIpAddr(resquest);
-				logger.info("[疑似攻击],refer url:" + str + " 用户ip:" + ip);
-				throw new Exception();
-			}
-		}
-	}
+    /**
+     * 过滤 script 脚本攻击
+     * 
+     * @param resquest
+     * @return
+     * @throws Exception
+     */
+    @SuppressWarnings("unchecked")
+    protected void replaceScript(HttpServletRequest resquest) throws Exception {
+        Enumeration<String> e = resquest.getParameterNames();
+        while (e.hasMoreElements()) {
+            String str = (String) e.nextElement();
+            String value = resquest.getParameter(str);
+            if (value.indexOf("script") != -1 || value.indexOf("javascript") != -1
+                    || value.indexOf("eval") != -1) {
+                String ip = WebUtils.getIpAddr(resquest);
+                logger.info("[疑似攻击],refer url:" + str + " 用户ip:" + ip);
+                throw new Exception();
+            }
+        }
+    }
 }
